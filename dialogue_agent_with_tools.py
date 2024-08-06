d651f786b3deaa9cb4fb7a3d8282c0f159189f81
Shyamji Pandey <154713663+ShyamRV@users.noreply.github.com>
2024-08-06 23:28:06 +0530

    Add files via upload

---
+++ dialogue_agent_with_tools.py
@@ -0,0 +1,76 @@
+from typing import List, Optional
+from xagent import XAgent  # Import XAgent
+from langchain.schema import AIMessage, SystemMessage
+from langchain_community.chat_models import ChatOpenAI
+
+from agents.agent_simulations.agent.dialogue_agent import DialogueAgent
+from agents.conversational.output_parser import ConvoOutputParser
+from config import Config
+from memory.zep.zep_memory import ZepMemory
+from services.run_log import RunLogsManager
+from typings.agent import AgentWithConfigsOutput
+
+class DialogueAgentWithTools(DialogueAgent):
+    def __init__(
+        self,
+        name: str,
+        agent_with_configs: AgentWithConfigsOutput,
+        system_message: SystemMessage,
+        model: ChatOpenAI,
+        tools: List[any],
+        session_id: str,
+        sender_name: str,
+        is_memory: bool = False,
+        run_logs_manager: Optional[RunLogsManager] = None,
+        **tool_kwargs,
+    ) -> None:
+        super().__init__(name, agent_with_configs, system_message, model)
+        self.tools = tools
+        self.session_id = session_id
+        self.sender_name = sender_name
+        self.is_memory = is_memory
+        self.run_logs_manager = run_logs_manager
+
+    def send(self) -> str:
+        """
+        Applies the chatmodel to the message history
+        and returns the message string
+        """
+
+        memory: ZepMemory
+
+        memory = ZepMemory(
+            session_id=self.session_id,
+            url=Config.ZEP_API_URL,
+            api_key=Config.ZEP_API_KEY,
+            memory_key="chat_history",
+            return_messages=True,
+        )
+
+        memory.human_name = self.sender_name
+        memory.ai_name = self.agent_with_configs.agent.name
+        memory.auto_save = False
+
+        callbacks = []
+
+        if self.run_logs_manager:
+            self.model.callbacks = [self.run_logs_manager.get_agent_callback_handler()]
+            callbacks.append(self.run_logs_manager.get_agent_callback_handler())
+
+        agent = XAgent(
+            tools=self.tools,
+            llm=self.model,
+            agent_kwargs={
+                "system_message": self.system_message.content,
+                "output_parser": ConvoOutputParser(),
+            },
+            verbose=True,
+        )
+
+        prompt = "\n".join(self.message_history + [self.prefix])
+
+        res = agent.run(input=prompt)
+
+        message = AIMessage(content=res)
+
+        return message.content
